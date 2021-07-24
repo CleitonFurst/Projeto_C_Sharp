3c43b452caefd98153c127387f82bf124f4c46d6
cleitonfurst <furstcleiton@gmail.com>
2021-07-23 23:10:56 -0300

    First commit

---
+++ Projeto_Final/Projeto_Final/Controllers/AnimeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto_Final.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Final.Controllers
+{
+    public class AnimeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(getAnimes());
+        }
+        List<Anime> getAnimes()
+        {
+            List<Anime> listaAnimes = new List<Anime>();
+            listaAnimes.Add(new Anime
+            {
+                Id = 1,
+                Nome = "Nanatsu no Taizai",
+                Sinopse = "A história segue os “Sete Pecados Capitais”," +
+                                                                                    "um grupo maligno de cavaleiros que conspiraram " +
+                                                                                    "para derrubar o reino de Britânia, " +
+                                                                                    "supostamente foram erradicados pelos Cavaleiros Divinos," +
+                                                                                    " embora ainda existam rumores de que eles estão vivos." +
+                                                                                    "Dez anos depois," +
+                                                                                    " os Cavaleiros Divinos realizaram um golpe de estado e " +
+                                                                                    "assassinaram o rei," +
+                                                                                    "  se tornando os novos e tiranos governantes do reino.Elizabeth, " +
+                                                                                    "a única filha do rei," +
+                                                                                    " sai em uma jornada para encontrar os “Sete Pecados Capitais”, " +
+                                                                                    "e recrutá-los para que possam ajudar a tomar o reino de volta.",
+                Episodio = 96,
+                Temporada = 4
+            });
+            listaAnimes.Add(new Anime
+            {
+                Id = 2,
+                Nome = "One Piece",
+                Sinopse = "One Piece Todos os Episodios Online, " +
+                                                                              "Assistir One Piece Anime Completo, Assistir One Piece Online." +
+                                                                              "Houve um homem que conquistou tudo aquilo que o mundo tinha a oferecer," +
+                                                                              " o lendário Rei dos Piratas," +
+                                                                              "Gold Roger.Capturado e condenado à execução pelo Governo Mundial," +
+                                                                              "suas últimas palavras lançaram legiões aos mares. “Meu tesouro ? Se quiserem," +
+                                                                              "podem pegá - lo.Procurem - no! Ele contém tudo que este mundo pode oferecer!”." +
+                                                                              "Foi a revelação do maior tesouro, o One Piece, cobiçado por homens de todo o mundo," +
+                                                                              "sonhando com fama e riqueza imensuráveis… Assim começou a Grande Era dos Piratas!",
+                Episodio = 983,
+                Temporada = 21
+            });
+            return listaAnimes;
+        }
+    }
+    
+}
---
+++ Projeto_Final/Projeto_Final/Models/Anime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Final.Models
+{
+    public class Anime
+    {
+        [Display(Name = "#")]
+        public int Id { get; set; }
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+        [Display(Name = "Sinopse")]
+        public string Sinopse { get; set; }
+        [Display(Name = "Episodio")]
+        public int Episodio { get; set; }
+        [Display(Name = "Temporada")]
+        public int Temporada { get; set; }
+
+    }
+}
